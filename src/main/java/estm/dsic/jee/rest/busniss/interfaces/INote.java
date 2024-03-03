736282f3d31d58b20b0e76aa92c6265421659319
oumnou <oumaimanouini08@gmail.com>
2024-03-03 22:01:53 +0100

    Adding teatcher's  archetype

---
+++ src/main/java/estm/dsic/jee/rest/busniss/interfaces/INote.java
@@ -0,0 +1,9 @@
+package estm.dsic.jee.rest.busniss.interfaces;
+
+import estm.dsic.jee.rest.model.Note;
+
+public interface INote {
+    Note addNote(Note note);
+    Note deleteNote(Note note);
+    Note updateNote(Note note);
+}
---
+++ src/main/java/estm/dsic/jee/rest/busniss/interfaces/services/NoteServices.java
@@ -0,0 +1,26 @@
+package estm.dsic.jee.rest.busniss.interfaces.services;
+
+import estm.dsic.jee.rest.busniss.interfaces.INote;
+import estm.dsic.jee.rest.model.Note;
+
+public class NoteServices implements INote{
+
+    @Override
+    public Note addNote(Note note) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addNote'");
+    }
+
+    @Override
+    public Note deleteNote(Note note) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteNote'");
+    }
+
+    @Override
+    public Note updateNote(Note note) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateNote'");
+    }
+    
+}
